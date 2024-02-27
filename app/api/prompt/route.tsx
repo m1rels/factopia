584f649e6737943e600052913d1fcc324b2a28e7
Mirel Korajac <mirelkorajac@gmail.com>
2024-02-27 19:18:44 +0100

    Make API Endpoint dynamic

--- app/api/prompt/route.tsx
+++ app/api/prompt/route.tsx
@@ -2,17 +2,15 @@ import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
 export const GET = async (request: Request) => {
-    try {
-        await connectToDB();
+  try {
+    await connectToDB();
 
-        const prompts = await Prompt.find({}).populate("creator");
+    const prompts = await Prompt.find({}).populate("creator");
 
-        if (prompts) {
-            const response = new Response(JSON.stringify(prompts), {status: 200})
-            return response;
-        }
+    const response = new Response(JSON.stringify(prompts), { status: 200 });
 
-    } catch (error) {
-        return new Response("Failed to fetch all prompts", { status: 500 })
-    }
-}
+    return response;
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+};
