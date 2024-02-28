fcaef13632456feedac390aa10363f2d58ecbdbe
Mirel Korajac <mirelkorajac@gmail.com>
2024-02-28 16:25:12 +0100

    Make API Route dynamic

--- app/api/prompt/route.tsx
+++ app/api/prompt/route.tsx
@@ -1,20 +1,20 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
-export const GET = async (request: Request) => {
+export const dynamic = 'force-dynamic'
+
+export const GET = async (request: any) => {
   try {
     await connectToDB();
 
     const prompts = await Prompt.find({}).populate("creator");
-
-    const url = new URL(request.url);
 
     if (prompts) {
       const response = new Response(JSON.stringify(prompts), { status: 200 });
       return response;
     }
 
-    
+
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
