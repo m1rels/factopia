ab1394f88e39dc91d9de8f51029946d2e3c94fb1
Mirel Korajac <mirelkorajac@gmail.com>
2024-03-03 19:35:47 +0100

    Remove if statement

--- app/api/prompt/route.tsx
+++ app/api/prompt/route.tsx
@@ -9,9 +9,7 @@ export const GET = async (request: any) => {
 
     const prompts = await Prompt.find({}).populate("creator");
 
-    if (prompts) {
-      return new Response(JSON.stringify(prompts), { status: 200 });
-    }
+    return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
