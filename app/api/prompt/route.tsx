d0d7e439aa4298f88bb5a1c5aa8dc32604ad0e78
Mirel Korajac <mirelkorajac@gmail.com>
2024-03-06 13:11:23 +0100

    Remove boolean flag

--- app/api/prompt/route.tsx
+++ app/api/prompt/route.tsx
@@ -5,16 +5,10 @@ export const dynamic = "force-dynamic";
 
 export const GET = async (request: any) => {
   try {
-    const isConnected = await connectToDB();
+    await connectToDB();
+    const prompts = await Prompt.find({}).populate("creator");
 
-    if (isConnected) {
-      const prompts = await Prompt.find({}).populate("creator");
-
-      return new Response(JSON.stringify(prompts), { status: 200 });
-    } else {
-      // Handle the case where the database connection is not successful
-      return new Response("Database connection failed", { status: 500 });
-    }
+    return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
