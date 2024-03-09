06bba8eb1f8a21711504d1818596e63c5119cca2
Mirel Korajac <mirelkorajac@gmail.com>
2024-03-09 21:05:18 +0100

    Fix Error with MissingSchemaError

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -1,11 +1,13 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
+import User from "@models/user";
 
 export const dynamic = "force-dynamic";
 
 export const GET = async (request: any) => {
   try {
     await connectToDB();
+
     const prompts = await Prompt.find({}).populate("creator");
 
     return new Response(JSON.stringify(prompts), { status: 200 });
